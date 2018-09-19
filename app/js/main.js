e18e5e041bfb51fa652b8b9ea5f0d772e8b81f06
Kunal Mishra <kunmishra2599@gmail.com>
2018-09-19 14:29:13 +0800

    Changed icons, rearranged FAQs

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 window.onscroll = () => {
   const nav = document.querySelector('.navbar');
-  if (this.scrollY <= 500) nav.className = 'nav navbar navbar-expand-lg navbar-light fixed-top';
+  if (this.scrollY <= 250) nav.className = 'nav navbar navbar-expand-lg navbar-light fixed-top';
 
   else nav.className = 'nav navbar navbar-expand-lg navbar-light fixed-top on-scroll';
 };
